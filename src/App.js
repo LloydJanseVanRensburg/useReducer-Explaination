bd5067dc700b297e3658d8272765361c5b35936e
Lloyd <lloydjvrensburg@gmail.com>
2020-08-18 23:07:33 +0200

    Final useReducer Code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer } from "react";
+import "./App.css";
+
+const reducerFunction = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "decrement":
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case "initCount":
+      return {
+        ...state,
+        count: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  count: 0,
+};
 
 function App() {
+  const [input, setInput] = useState(0);
+  const [state, dispatch] = useReducer(reducerFunction, initialState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Reducer Example</h1>
+
+      <div>
+        <label>Start Count:</label>
+        <input
+          type="number"
+          onChange={(e) => setInput(parseInt(e.target.value))}
+          value={input}
+        />
+        <br />
+        <button onClick={() => dispatch({ type: "initCount", payload: input })}>
+          Initialize Counter
+        </button>
+      </div>
+
+      <p>{state.count}</p>
+      <button onClick={() => dispatch({ type: "increment" })}>increment</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>decrement</button>
     </div>
   );
 }
